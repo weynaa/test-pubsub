3df88bb182423c27aafd19dada9e3042e067aaef
Michael Wechner <michael.wechner@tum.de>
2025-10-31 22:36:22 +0100

    wip

--- client.cpp
+++ client.cpp
@@ -1,5 +1,17 @@
+#include <agrpc/asio_grpc.hpp>
 
-int main(int argc, const char** argv){
+#include "pubsub.grpc.pb.h"
 
-	return 0;
+int main(int argc, const char **argv) {
+  agrpc::GrpcContext grpc_context;
+  grpc_context.work_started();
+
+  const auto grpc_context_thread = std::jthread([&]() { grpc_context.run(); });
+  auto scope_exit = std::shared_ptr<agrpc::GrpcContext>(
+      &grpc_context,
+      [](agrpc::GrpcContext *context) { context->work_finished(); });
+  using ClientRPC = agrpc::ClientRPC<&PubSub::AsyncService::Requestsubscribe>;
+
+
+  return 0;
 }
